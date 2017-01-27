0ea089c2f1de52555c19b111b06c2b99fd6eccbd
normaledezma <normaledezma@10.34.145.40>
2017-01-27 12:03:22 -0600

    Perimetro y superficie

---
+++ src/spp2/nledezmam/t01/p2/SPP2NLedezmaMT01P2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.nledezmam.t01.p2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author normaledezma
+ */
+public class SPP2NLedezmaMT01P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Calculadora de perímeto y superficie de un rectángulo");
+        //Variables
+        double base, altu, sup, per; 
+        
+        //Objeto para recibir datos
+        Scanner tecl = new Scanner (System.in);
+        
+        //Asignar valores
+         System.out.println("Introduce cuanto mide la base:");
+        base=tecl.nextDouble();
+         System.out.println("Introduce cuanto mide la altura:");
+        altu=tecl.nextDouble();
+        
+        //Operaciones
+        sup = base * altu ;
+        per = (base * 2) + (altu *2);
+        
+        //Mostrar resultado
+        System.out.println("Resultados:" +
+                "\n"+ "Superficie: " + sup +
+                "\n"+ "Perímetro: " + per);
+    }
+    
+}
